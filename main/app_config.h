5c669130123b1aa4a490b0b9362e6039d96db112
Raki Delmoro <rakidelmoro@gmail.com>
2026-03-06 16:23:32 +0800

    Main configurations

---
+++ main/app_config.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* WiFi Configuration */
+#define WIFI_SSID           ""
+#define WIFI_PASSWORD       ""
+#define WIFI_MAX_RETRY      5
+
+/* MQTT Configuration */
+#define MQTT_BROKER_URL     ""
+#define MQTT_CLIENT_ID      ""
+#define MQTT_USERNAME       ""
+#define MQTT_PASSWORD       ""
+
+/* MQTT Topics */
+#define TOPIC_BRIGHT_STATE  "sensecap/bright_switch/state"
+#define TOPIC_RELAX_STATE   "sensecap/relax_switch/state"
+#define TOPIC_SENSORS       "esp/water-level"
+
+#ifdef __cplusplus
+}
+#endif
